54ad0bdf562f1dd9cbe6df2e4532462aabfd809f
hamed-jamali-software <hamed.jamali.software@gmail.com>
2023-10-26 09:02:44 +0200

    1-2 input

---
+++ 1-2-input.py
@@ -0,0 +1,8 @@
+
+print("hello world!")
+name = input("What is your name? ")
+print("Nice to meet you,", name)
+age = int(input('What is your age?\n'))
+
+age = age + 1
+print("Your age is: "+str(age))
