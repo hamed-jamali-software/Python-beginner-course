2dbf03981100e05127f373794cdc146c89e98ad3
hamed-jamali-software <hamed.jamali.software@gmail.com>
2023-10-26 08:59:55 +0200

    1-1 hello world

---
+++ 1-1-hello-world.py
@@ -0,0 +1,26 @@
+#Multi-Line Comment
+
+""" First Python Program
+    Just to see if we can run python3 successfully
+"""
+
+print("My name is Hamed.")
+
+string = "My name is Hamed."
+print(string)
+
+name = "Hamed"
+print("My name is " + name + ".")
+
+print("This is how you can draw a triangle")
+
+print("|\    ")
+print("| \   ")
+print("|  \  ")
+print("|___\ ")
+
+# printing 'x' as a string
+x = 20
+print(str(x))
+
+
