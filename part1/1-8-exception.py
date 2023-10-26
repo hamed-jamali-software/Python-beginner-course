1e840a1b70138898ca0a34c78be3d2ba1d8d4a9d
hamed-jamali-software <hamed.jamali.software@gmail.com>
2023-10-26 09:29:37 +0200

    beginner part 2

---
+++ part1/1-8-exception.py
@@ -0,0 +1,21 @@
+# x = 10 / 0
+
+# Exceptions provide a way of catching errors and then handling them in
+# a separate section of the code to group them together
+try:
+    x = 10 / 0
+except:
+    print("Well that didn't work!")
+
+# You can also catch specific exceptions
+try:
+    answer = input("What should I divide 10 by?")
+    num = int(answer)
+    print(10 / num)
+except ZeroDivisionError as e:
+    print("You can't divide by zero!")
+except ValueError as e:
+    print("You didn't give me a valid number!")
+    print(e)
+finally:
+    print("The finally section always runs")
